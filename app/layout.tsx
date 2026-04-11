5a94966532f62296f75c3d822d0e9fd8c58c0540
Yüksel Kapan <yukselkapan@gmail.com>
2026-04-12 01:04:21 +0300

    feat: add Google Analytics integration to track user interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { Geist_Mono, Instrument_Sans } from "next/font/google";
+import Script from "next/script";
 
 import '@xyflow/react/dist/style.css';
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+
+const GOOGLE_ANALYTICS_ID = "G-H4K593QJ6T"
 
 const instrumentSans = Instrument_Sans({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -24,6 +27,21 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", instrumentSans.variable)}
     >
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `}
+        </Script>
+      </head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
