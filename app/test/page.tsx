1245f5573a1fe6b4b70791da297e6b1dcbb4dd3d
Yüksel Kapan <yukselkapan@gmail.com>
2026-04-19 12:19:54 +0300

    refactor(shader): simplify drawScene and initBuffers functions, remove color handling

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,92 +1,68 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { drawScene } from "@/shaders/draw-scene";
+import { ProgramInfo, drawScene } from "@/shaders/draw-scene";
 import { initBuffers } from "@/shaders/init-buffers";
 import { initShaderProgram } from "@/shaders/init-shader-program";
 import { useEffect, useRef } from "react";
 
 const size = 16;
 const frames = 16;
-const scale = 1;
-
-// Vertex shader program
+const scale = 10;
 
 const vsSource = `
-    attribute vec4 aVertexPosition;
-    attribute vec4 aVertexColor;
+    attribute vec2 aVertexPosition;
 
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
-
-    varying lowp vec4 vColor;
+    varying highp vec2 vUv;
 
     void main(void) {
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-      vColor = aVertexColor;
+      vUv = aVertexPosition;
+      gl_Position = vec4(
+        aVertexPosition.x * 2.0 - 1.0,
+        1.0 - aVertexPosition.y * 2.0,
+        0.0,
+        1.0
+      );
     }
   `;
 
-
-// Fragment shader program
 const fsSource = `
-    varying lowp vec4 vColor;
+    precision mediump float;
+
+    varying highp vec2 vUv;
 
     void main(void) {
-      gl_FragColor = vColor;
+      gl_FragColor = vec4(vUv.x, vUv.y, 1.0 - vUv.x, 1.0);
     }
   `;
 
-
-export type ProgramInfo = {
-    program: WebGLProgram;
-    attribLocations: {
-        vertexPosition: number;
-        vertexColor: number;
-    };
-    uniformLocations: {
-        projectionMatrix: WebGLUniformLocation | null;
-        modelViewMatrix: WebGLUniformLocation | null;
-    };
-};
 export default function Page() {
     const canvasRef = useRef<HTMLCanvasElement>(null!);
-    function paint() {
-        const gl = canvasRef.current.getContext("webgl")!;
-        gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
 
+    useEffect(() => {
+        const gl = canvasRef.current.getContext("webgl");
+        if (!gl) {
+            return;
+        }
+
+        gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
         const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-        const programInfo = {
+        const programInfo: ProgramInfo = {
             program: shaderProgram,
             attribLocations: {
                 vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-                vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
-            },
-            uniformLocations: {
-                projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-                modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
             },
         };
-        // objects we'll be drawing.
         const buffers = initBuffers(gl);
 
-        // Draw the scene
         drawScene(gl, programInfo, buffers);
-
-    }
-    useEffect(() => {
-        paint();
     }, []);
+
     return (
         <div>
             <h1>Test</h1>
-            <Button onClick={paint}>Paint</Button>
             <div className="bg-gray-500 w-min h-min p-1">
                 <canvas ref={canvasRef} width={size} height={size * frames} className="border border-gray-700" style={{
-                    //pixel art should look nearest neighbour
                     width: size * scale,
                     height: size * frames * scale,
                     imageRendering: "pixelated"
